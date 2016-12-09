1f439cc3ffed730c9335a6e0749f9f05c6c68b3d
Jon Olson <jonolson64@outlook.com>
2016-12-09 12:39:07 -0500

    Added 06_timer files

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,32 @@
 class Timer
-  #write your code here
+  def initialize
+  	@seconds = 0
+  end
+
+  def seconds
+  	@seconds
+  end
+
+  def seconds=(val)
+  	@seconds = val
+  end
+
+  def time_string
+  	out = ""
+  	hrs = @seconds/3600
+  	remainder = @seconds - hrs*3600
+  	mins = remainder/60
+  	secs = remainder - mins*60
+  	out += padded(hrs) + ":"
+  	out += padded(mins) + ":"
+  	out += padded(secs)
+  end
+
+  def padded(val)
+  	out = ""
+  	if (val.to_i < 10)
+  		out = "0"
+  	end
+  	out += val.to_s
+  end
 end
